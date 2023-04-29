3f3eceeb9a8f7e4d28aadef805f21fcdbb19f094
Chetan Patil <patilchetan2110@gmail.com>
2023-04-29 23:47:25 +0530

    backend initial steps done -- api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       {/* <EnterpriseDashboard /> */}
       {/* <LandingPage /> */}
-      <SignIn />
+      {/* <SignIn /> */}
       <SignUp />
     </div>
   );
